7ee7037d87e1c98485bb4e1da920d46fd46274e0
Mohammad Abumarar <mohammed.osama.abumarar@gmai.com>
2026-01-09 13:50:12 +0300

    fix: import api_view and permission_classes

--- itas-backend/core/views.py
+++ itas-backend/core/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import get_user_model, authenticate
 from django.db.models import Q
 
 from rest_framework import viewsets, status, exceptions
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
