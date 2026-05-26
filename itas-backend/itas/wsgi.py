e67541fba2d92616d5fe2c3781c362d9a3d140d9
Abumarar <abumarar@example.com>
2026-05-26 14:13:12 +0300

    feat: configure dynamic Django settings selection based on RENDER environment variable

--- itas-backend/itas/wsgi.py
+++ itas-backend/itas/wsgi.py
@@ -6,6 +6,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "itas.settings.development")
+settings_module = "itas.settings.production" if os.environ.get("RENDER") else "itas.settings.development"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_wsgi_application()
