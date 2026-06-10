7a21de549e64353f1431747e11750d9a9e4ec830
Mohammad Abumarar <mo.abumarar@gmail.com>
2026-06-10 11:53:10 +0300

    refactor: remove obsolete temporary testing scripts and sample data files

--- itas-backend/test_router.py
+++
@@ -1,8 +0,0 @@
-import os, django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'itas.settings')
-django.setup()
-
-from core.urls import router
-for prefix, viewset, basename in router.registry:
-    if prefix == 'employees':
-        print("EmployeeViewSet module:", viewset.__module__)
